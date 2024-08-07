99907027bd89464fbd6a00291596caa3ea36621d
Prayas Gautam <prayas98@gmail.com>
2024-08-07 23:59:11 +0530

    Managing templates and using CONTEXT to pass data from DB to webpage.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'User1',
+        'title': 'Blog Post 1',
+        'content': 'First blog post',
+        'date_posted': '30 july 2024'
+    },
+    {
+        'author': 'User2',
+        'title': 'Blog Post 2',
+        'content': 'Second blog post',
+        'date_posted': '30 july 2024'
+    },
+]
 
-# Create your views here.
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
