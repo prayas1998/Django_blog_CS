3a1c7425a5c816cdf59bacfe5e829ed4ac78cc1b
Prayas Gautam <prayas98@gmail.com>
2024-08-10 18:28:02 +0530

    Creating Post Model, DB Migrations, using actual DB instead of dummy data.

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-    {
-        'author': 'User1',
-        'title': 'Blog Post 1',
-        'content': 'First blog post',
-        'date_posted': '30 july 2024'
-    },
-    {
-        'author': 'User2',
-        'title': 'Blog Post 2',
-        'content': 'Second blog post',
-        'date_posted': '30 july 2024'
-    },
-]
+from blog.models import Post
+
+
+# posts = [
+#     {
+#         'author': 'User1',
+#         'title': 'Blog Post 1',
+#         'content': 'First blog post',
+#         'date_posted': '30 july 2024'
+#     },
+#     {
+#         'author': 'User2',
+#         'title': 'Blog Post 2',
+#         'content': 'Second blog post',
+#         'date_posted': '30 july 2024'
+#     },
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
