81ab55d7f121de6dfc9702554db7a0e0e7f7cc92
Prayas Gautam <prayas98@gmail.com>
2024-08-10 18:41:12 +0530

    registering models into admin.py to display them on admin site.

--- blog/views.py
+++ blog/views.py
@@ -4,22 +4,6 @@ from django.http import HttpResponse
 from blog.models import Post
 
 
-# posts = [
-#     {
-#         'author': 'User1',
-#         'title': 'Blog Post 1',
-#         'content': 'First blog post',
-#         'date_posted': '30 july 2024'
-#     },
-#     {
-#         'author': 'User2',
-#         'title': 'Blog Post 2',
-#         'content': 'Second blog post',
-#         'date_posted': '30 july 2024'
-#     },
-# ]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all(),
