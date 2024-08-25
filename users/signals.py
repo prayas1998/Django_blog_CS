38115653306e1a369ce980ba23cb629aa1a25a85
Prayas Gautam <prayas98@gmail.com>
2024-08-25 20:35:51 +0530

    Serving images uploaded by users. Using signals in order to automatically create profile page for newly registered users.

---
+++ users/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User  # Sender of signal
+from django.dispatch import receiver  # receiver of signal
+from .models import Profile
+
+
+@receiver(post_save,sender=User)  # When user is saved send this signal. This signal is received triggers create_profile signal
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
