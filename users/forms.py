15eb6827a858c8426ebd2047ab326a90afb95ed0
Prayas Gautam <prayas98@gmail.com>
2024-08-29 23:20:58 +0530

    Update User Profile, resizing images and delete old images

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,9 +12,19 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']  # The fields that will be displayed
 
 
+class UserUpdateForm(forms.ModelForm):  # Only to update Email and Username
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 
-
+class ProfileUpdateForm(forms.ModelForm):  # To update profile picture(Image)
+    # No additional fields needed to directly jumping to class Meta:
+    class Meta:
+        model = Profile
+        fields = ['image']
 
 
 '''
