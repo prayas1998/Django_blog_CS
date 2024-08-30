b52cd4b3884857c6aff3feeceb94efd25af4aeed
Prayas Gautam <prayas98@gmail.com>
2024-08-30 15:10:43 +0530

    Understanding Class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
+
+# urlpatterns = [
+#     path('', views.home, name='blog-home'),  # Imported the home function from 'views'
+#     path('about/', views.about, name='blog-about'),
+# ]
+
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),  # Imported the home function from 'views'
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from blog.models import Post
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
@@ -10,6 +11,18 @@ def home(request):
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
+
+
+# Creating a class-based view similar to 'home' function based view above.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<ViewType>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # To show latest blogs on top
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
