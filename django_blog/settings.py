1ff8c41522c2681f46c296a85a85e4c55720ace3
Prayas Gautam <prayas98@gmail.com>
2024-09-19 02:56:56 +0530

    Accessing environment variables throught .env file

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,9 +135,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER_DJANGO')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS_DJANGO')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
-# print("These are username and passwords:")
-# print(EMAIL_HOST_USER)
-# print(EMAIL_HOST_PASSWORD)
+print("These are username and passwords:")
+print(EMAIL_HOST_USER)
+print(EMAIL_HOST_PASSWORD)
