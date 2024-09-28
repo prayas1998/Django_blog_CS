328730bbc2724a5be1081a99fea592b1876f8c2b
Prayas Gautam <prayas98@gmail.com>
2024-09-28 17:34:48 +0530

    Added image NSFW check before uploading to S3 bucket

--- helper.py
+++ helper.py
@@ -1,4 +1,7 @@
+from PIL import Image
 from transformers import pipeline
+import io
+
 
 def check_profanity(text):
     classifier = pipeline("text-classification", model="eliasalbouzidi/distilbert-nsfw-text-classifier")
@@ -7,3 +10,17 @@ def check_profanity(text):
         return False
     result = classifier(text)
     return result[0]['label'] == 'nsfw' and result[0]['score'] > 0.75
+
+
+def check_nsfw_image(image_file):
+    # Open the image file from memory (for files uploaded via Django forms)
+    image = Image.open(image_file)
+
+    # Load the Hugging Face NSFW detection model
+    classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
+
+    # Run the image through the classifier
+    result = classifier(image)
+
+    # Check if the label is NSFW with a score greater than a threshold (e.g., 0.75)
+    return any(item['label'] == 'nsfw' and item['score'] > 0.75 for item in result)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.http import request
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from helper import check_nsfw_image
 
 
 def register(request):
@@ -31,6 +33,12 @@ def profile(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
+            # Check if the uploaded profile picture is NSFW
+            image_file = request.FILES.get('image')  # Fetch the image from the form
+            if image_file and check_nsfw_image(image_file):
+                messages.error(request, "The uploaded image contains inappropriate content.", extra_tags='danger')
+                return redirect('profile')
+            # If image is clean, save the forms
             u_form.save()
             p_form.save()
             messages.success(request, f"Your profile has been updated!")
