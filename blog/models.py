a2feceea8b3a63323ad026b029a24187ebd42e0a
Prayas Gautam <prayas98@gmail.com>
2025-03-19 01:37:47 +0530

    Changed title max length

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=1000)
     content = models.TextField()
     date_posted = models.DateTimeField(
         default=timezone.now
